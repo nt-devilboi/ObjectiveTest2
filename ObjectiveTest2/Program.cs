15c1a32028713bbbe34b9b8952bd500fdbcea2b8
devilboi95 <turnickii.n@gmail>
2024-02-21 09:18:17 +0500

    Initial commit

---
+++ ObjectiveTest2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+class Server
+{
+    private int _count;
+    private readonly object _lock = new();
+
+    public Server()
+    {
+        _count = 0;
+    }
+
+    public int GetCount()
+    {
+        lock (_lock)
+        {
+            return _count;
+        }
+    }
+
+    public void AddToCount(int count)
+    {
+        lock (_lock) // пока count меняется, GetCount будет ждать.
+        {
+            _count += count;
+        }
+    }
+}
